d403f8635fae83c539970061e08115cf058d5766
C515 <87876690+C5fifteen@users.noreply.github.com>
2022-12-22 19:57:48 -0800

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,15 @@
+# Variables
+
+# Variables are used to store values in Python.
+# They can be created using the "=" operator.
+
+name = "John"
+age = 30
+
+# You can use variables in expressions and assignments.
+new_age = age + 1
+
+# You can also print the value of a variable using the print function.
+print(name)
+print(age)
+print(new_age)
