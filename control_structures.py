5543fe77f5b919d86efc16124e789c519963bb47
C515 <87876690+C5fifteen@users.noreply.github.com>
2022-12-22 20:03:10 -0800

    Create control_structures.py

---
+++ control_structures.py
@@ -0,0 +1,25 @@
+# Control Structures
+
+# Control structures are used to control the flow of execution of a program.
+# Python has several built-in control structures, including if-else statements, which are used to execute a block of code based on a condition.
+
+x = 10
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is not greater than 5")
+
+# For loops are used to iterate over a sequence of values.
+# The range function generates a sequence of numbers.
+for i in range(5):
+    print(i)
+
+# You can also use for loops to iterate over a list.
+for fruit in fruits:
+    print(fruit)
+
+# While loops are used to execute a block of code repeatedly until a condition is met.
+i = 0
+while i < 5:
+    print(i)
+    i += 1
