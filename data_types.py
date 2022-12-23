b4caafdd2fc85c11cbd9ce3339b286150923b429
C515 <87876690+C5fifteen@users.noreply.github.com>
2022-12-22 19:58:41 -0800

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,21 @@
+# Data Types
+
+# Python has several built-in data types, including numbers, strings, and booleans.
+
+# Numbers can be integers or floating point values.
+a = 10
+b = 3.14
+
+# Strings are sequences of characters, and can be enclosed in single or double quotes.
+c = "Hello, world!"
+
+# Booleans represent True or False values.
+d = True
+e = False
+
+# You can use the type function to check the data type of a value.
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
